45123bc31771782767194bae32e15f90e5303524
nkunic <natasa.kunic@gmail.com>
2022-04-12 20:23:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-6r3txk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-6r3txk)
---
+++ index.js
@@ -0,0 +1,138 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+
+/***** ALERT *****/
+//alert("Hello Javascript");  // String
+//alert("Hello again");
+//alert(broj); // Variable
+
+/***** DATA TYPES *****/
+typeof 'Jovan'; // vraća "string"
+typeof 3.14; // vraća "number"
+typeof NaN; // vraća "number"
+typeof false; // vraća "boolean"
+typeof [1, 2, 3, 4]; // vraća "object" ???
+typeof { name: 'Jovan', age: 34 }; // vraća "object"
+typeof new Date(); // vraća "object"
+typeof function () {}; // vraća "function"
+typeof myCar; // vraća "undefined"
+typeof null; // vraća "object"
+
+/***** VARIABLES *****/
+//var broj = 12; // number
+//console.log(broj);
+//var text = 'Učim Javascript'; // string
+//console.log(text);
+
+/***** STRING *****/
+//var name = 'Nataša';
+//var job = 'developer';
+//var nj = name + job;
+//console.log(nj);
+
+//var text2 = "Danas je \"divan\" dan";
+//var text3 = 'Danas je \n novi \t dan';
+//console.log(text2);
+//console.log(text3);
+
+/***** NUMBER *****/
+//var num1 = 6;
+//var num2 = 12.3;
+//var str1 = "Nataša";
+//var sum = num1 + num2;
+//var sum = str1 + num1;
+//console.log(sum);
+
+/***** UNDEFINED & NULL *****/
+//var car;
+//var car = null;
+//console.log(car);
+//var car = 'Volvo';
+//console.log(car);/
+//console.log(undefined === null); // false
+
+/*****  ARRAY *****/
+//var cars = ['Škoda', 'Volvo', 'Audi'];
+//console.log(cars);
+//var car = cars[0];
+//console.log(car);
+//console.log(cars[1]);
+
+//var cars = ['Škoda', 'Volvo', 'Audi', 2, null, undefined, [1, 2, 3]];
+//console.log(cars[4]);
+//console.log(cars[5]);
+//console.log(cars[6]);
+//console.log(cars[6][1]);
+
+/*****  OBJECT *****/
+//var grad = {
+//  ime: 'Beograd',
+//  brojStanovnika: 2000000,
+//  reke: ['Dunav', 'Sava'],
+//};
+//console.log(grad.ime);
+//console.log(grad.ime + "ima" + grad.brojStanovnika + "stanovnika");
+//console.log(grad.reke[0]);
+//grad.staroIme = 'Singidunum';
+//console.log(grad);
+//console.log(grad["ime"]);
+
+//var knjiga = {
+//	ime : "JavaScript",
+//	pisac : {
+//		ime : "Danilo",
+//		prezime : "Vesović"
+//	}
+//}
+//console.log(knjiga.pisac.ime);
+
+/***** FUNCTION *****/
+//function display() {
+//  var name = 'Js';
+//  console.log(name);
+//}
+//display();
+
+//function display() {
+//console.log(name2);
+//}
+//display(); // name2 is not defined
+
+//function display(a){
+//  console.log(a);
+//}
+//display(); // undefined
+
+//function display(a) {
+//  console.log(a);
+//}
+//display('Javascript');
+//display('Php');
+
+//function info(){
+//	console.log('Zdravo, Svete');
+//}
+//info();
+
+//function zbir (broj1,broj2){
+//	console.log(broj1+broj2);
+//}
+//zbir(31,90)
+
+//function naKvadrat(broj1) {
+//  var rezultat = broj1 * broj1;
+//  return rezultat;
+//}
+//console.log(naKvadrat(25)); // 625
+//var brojNaKvadrat = naKvadrat(25);
+//console.log(brojNaKvadrat); // 625
+
+//function pozdrav(ime) {
+//  var ceoPozdrav = 'Zdravo' + ' ' + ime + '!';
+//  return ceoPozdrav;
+//}
+//console.log(pozdrav('Juliska'));
+//var pozdravImenom = pozdrav('Juliska');
+//console.log(pozdravImenom);
