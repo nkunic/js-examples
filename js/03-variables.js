96af2fd86e58a06ecffb065497c53bcde5f81c60
nkunic <natasa.kunic@gmail.com>
2022-04-13 17:55:37 +0200

    1 file modified

--- js/03-variables.js
+++ js/03-variables.js
@@ -5,19 +5,60 @@ var text = 'Učim Javascript'; // string
 console.log(text);
 
 /*** SCOPE  ***/
-var ime = "global";
-function proveriScope(){
-	var ime = "local";
-	console.log(ime);
+var ime = 'global';
+function proveriScope() {
+  var ime = 'local';
+  console.log(ime);
 }
-proveriScope();              // "local"
-console.log(ime);            // "global"
+proveriScope(); // "local"
+console.log(ime); // "global"
 
-var ime = "global";
-function proveriScope(){
-	ime = "local";
-	console.log(ime);
+var ime = 'global';
+function proveriScope() {
+  ime = 'local';
+  console.log(ime);
 }
-proveriScope()              // "local"
-console.log(ime)            // "local"
+proveriScope(); // "local"
+console.log(ime); // "local"
 
+var car = 'Volvo'; // global
+console.log(car);
+function info() {
+  console.log(car);
+}
+info();
+
+function info() {
+  var cat = 'Školjkica'; // local
+  console.log(cat);
+}
+info();
+
+var cat = 'Žaklina'; // global
+function info() {
+  var cat = 'Školjkica'; // local
+}
+info();
+console.log(cat);
+
+var cat = 'Žaklina'; // global
+function info() {
+  var cat = 'Školjkica'; // local
+  console.log(cat);
+}
+info();
+console.log(cat);
+
+var cat = 'Žaklina'; // global
+function info() {
+  cat = 'Školjkica'; // global
+  console.log(cat);
+}
+info();
+console.log(cat);
+
+function info() {
+  console.log(animal); // undefined
+  var animal = 'Dog';
+}
+info();
