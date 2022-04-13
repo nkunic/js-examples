ee66768ed6c31d3e432bf580d1fb31d5b99f530f
nkunic <natasa.kunic@gmail.com>
2022-04-13 16:44:04 +0200

    1 file modified, 1 added

---
+++ js/11-example-quiz.js
@@ -0,0 +1,36 @@
+/***** QUIZ *****/
+
+var questions = [
+  {
+    text: 'JS je programski jezik?',
+    correctAnswer: true,
+  },
+  {
+    text: 'PHP je isto što i JS?',
+    correctAnswer: false,
+  },
+  {
+    text: 'CSS je stajling jezik?',
+    correctAnswer: true,
+  },
+];
+
+var questionIndex = 0;
+var userScore = 0;
+
+function askQuestion() {
+  var userAnswer = confirm(questions[questionIndex].text + "\nCancel = Ne     OK = Da ");
+  if(questions[questionIndex].correctAnswer === userAnswer) {
+    userScore++;
+  }
+  questionIndex++;
+  if(questionIndex !== questions.length) {
+    askQuestion();
+  }
+}
+askQuestion();
+// askQuestion();
+// askQuestion();
+
+//console.log("Odgovorili ste tačno na: " + userScore + " od " +  questions.length + " pitanja");
+alert("Odgovorili ste tačno na: " + userScore + " od " +  questions.length + " pitanja");
