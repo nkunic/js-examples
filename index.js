6f3e01cb82bc4d797be23c0f397b3d311ad768a1
nkunic <natasa.kunic@gmail.com>
2022-04-13 16:09:06 +0200

    1 file modified

--- index.js
+++ index.js
@@ -3,23 +3,27 @@ import './style.css';
 
 // Write Javascript code!
 
-
-
 var questions = [
   {
-    text: "JS je programski jezik?",
-    correctAnswer: true
+    text: 'JS je programski jezik?',
+    correctAnswer: true,
   },
   {
-    text: "PHP je isto što i JS?",
-    correctAnswer: false
+    text: 'PHP je isto što i JS?',
+    correctAnswer: false,
   },
   {
-    text: "CSS je stajling jezik?",
-    correctAnswer: true
-  }
-]
+    text: 'CSS je stajling jezik?',
+    correctAnswer: true,
+  },
+];
+
+var questionIndex = 0;
 
 function askQuestion() {
-  var userAnswer = confirm(questions[]);
+  var userAnswer = confirm(questions[questionIndex].text);
+  questionIndex++;
 }
+askQuestion();
+askQuestion();
+askQuestion();
