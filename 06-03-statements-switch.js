9d432c3141557f2bba6f94c51e089d8f308d02f1
nkunic <natasa.kunic@gmail.com>
2022-04-13 14:38:09 +0200

    2 files added, 1 deleted

--- 06-03-statements-switch.js
+++ 06-03-statements-switch.js
@@ -1,24 +1,3 @@
-/***** IF STATEMENT *****/
-// if (true) {
-//   console.log('Pogodak!');
-// } else {
-//   console.log('Greška!');
-// }
-// var password = 123;
-// if (password === 123) {
-//   console.log('Pogodak!');
-// } else {
-//   console.log('Greška!');
-// }
-// var password = prompt("Pogodite broj");
-// if (password === "123") {
-//   console.log('Pogodak!');
-// } else {
-//   console.log('Greška!');
-// }
-
-
-
 // var num1 = parseInt(prompt('Unesite broj 1'));
 // var num2 = parseInt(prompt('Unesite broj 2'));
 // var action = prompt('Unesite željenu radnju \n + - / *');
@@ -35,19 +14,6 @@
 // function mult() {
 //   console.log(num1 * num2);
 // }
-
-// if (action === "+") {
-//   add();
-// } else if (action === "-") {
-//   sub();
-// } else if (action === "/") {
-//   div();
-// } else if (action === "*") {
-//   mult();
-// } else {
-//   console.log("Pogrešan unos!");
-// }
-
 
 /***** SWITCH *****/
 // switch (action) {
