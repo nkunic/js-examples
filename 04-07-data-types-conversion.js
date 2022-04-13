348cdf7fedf2f748af6c128d44bf9bdf9f18541d
nkunic <natasa.kunic@gmail.com>
2022-04-13 14:33:53 +0200

    1 file modified, 1 added

---
+++ 04-07-data-types-conversion.js
@@ -0,0 +1,21 @@
+/***** DATA TYPES CONVERSION *****/
+//var num1 = prompt("Unesite prvi broj");
+//var num2 = prompt("Unesite drugi broj");
+//console.log(num1 + num2);
+
+//var num1 = prompt("Unesite prvi broj");
+//var num2 = prompt("Unesite drugi broj");
+//var num3 = parseInt(num1);
+//var num4 = parseInt(num2);
+//console.log(num3 + num4);
+
+//var num1 = parseInt(prompt('Unesite prvi broj'));
+//var num2 = parseInt(prompt('Unesite drugi broj'));
+//console.log(num1 + num2);
+
+//console.log("10" - 5);
+//console.log("Danilo" / 5); // NaN not an number
+
+// var names = ['Ana', 'Marija', 'Jovana'];
+// console.log(names);
+// console.log(names.toString());
--- index.js
+++ index.js
@@ -4,19 +4,6 @@ import './style.css';
 // Write Javascript code!
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 /***** IF STATEMENT *****/
 // if (true) {
 //   console.log('Pogodak!');
@@ -42,27 +29,6 @@ import './style.css';
 
 
 
-/***** DATA TYPES CONVERSION *****/
-//var num1 = prompt("Unesite prvi broj");
-//var num2 = prompt("Unesite drugi broj");
-//console.log(num1 + num2);
-
-//var num1 = prompt("Unesite prvi broj");
-//var num2 = prompt("Unesite drugi broj");
-//var num3 = parseInt(num1);
-//var num4 = parseInt(num2);
-//console.log(num3 + num4);
-
-//var num1 = parseInt(prompt('Unesite prvi broj'));
-//var num2 = parseInt(prompt('Unesite drugi broj'));
-//console.log(num1 + num2);
-
-//console.log("10" - 5);
-//console.log("Danilo" / 5); // NaN not an number
-
-// var names = ['Ana', 'Marija', 'Jovana'];
-// console.log(names);
-// console.log(names.toString());
 
 /***** FUNCTION *****/
 //function display() {
