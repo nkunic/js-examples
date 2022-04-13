a6f01a278ebf790cce3d5f834782afeb44de6ce1
nkunic <natasa.kunic@gmail.com>
2022-04-13 13:59:21 +0200

    1 file modified

--- index.js
+++ index.js
@@ -76,16 +76,12 @@ import './style.css';
 // } else {
 //   console.log('Greška!');
 // }
-var password = prompt("Pogodite broj");
-if (password === 123) {
-  console.log('Pogodak!');
-} else {
-  console.log('Greška!');
-}
-
-
-
-
+// var password = prompt("Pogodite broj");
+// if (password === "123") {
+//   console.log('Pogodak!');
+// } else {
+//   console.log('Greška!');
+// }
 
 /***** UNDEFINED & NULL *****/
 //var car;
@@ -200,3 +196,50 @@ if (password === 123) {
 //console.log(pozdrav('Juliska'));
 //var pozdravImenom = pozdrav('Juliska');
 //console.log(pozdravImenom);
+
+/***** PRACTICE *****/
+// var num1 = parseInt(prompt('Unesite broj 1'));
+// var num2 = parseInt(prompt('Unesite broj 2'));
+// var action = prompt('Unesite željenu radnju \n + - / *');
+
+// function add() {
+//   console.log(num1 + num2);
+// }
+// function sub() {
+//   console.log(num1 - num2);
+// }
+// function div() {
+//   console.log(num1 / num2);
+// }
+// function mult() {
+//   console.log(num1 * num2);
+// }
+
+// if (action === "+") {
+//   add();
+// } else if (action === "-") {
+//   sub();
+// } else if (action === "/") {
+//   div();
+// } else if (action === "*") {
+//   mult();
+// } else {
+//   console.log("Pogrešan unos!");
+// }
+
+// switch (action) {
+//   case '+':
+//     add();
+//     break;
+//   case '-':
+//     sub();
+//     break;
+//   case '/':
+//     div();
+//     break;
+//   case '*':
+//     mult();
+//     break;
+//   default:
+//     console.log('Pogrešan unos!');
+// }
