d556b270c9027a73d6bd5a845c9513220d329b9e
nkunic <natasa.kunic@gmail.com>
2022-04-14 10:32:06 +0200

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 import './style.css';
 
 // Write Javascript code!
+var count = 0;
+do {
+  console.log(count);
+  count++;
+}
+while (count == 20);
---
+++ js/08-loops.js
@@ -0,0 +1,14 @@
+/***** LOOPS *****/
+
+var count = 0;
+while (count < 10) {
+  console.log(count);
+  count++;
+}
+
+var count = 0;
+do {
+  console.log(count);
+  count++;
+}
+while (count < 10);
