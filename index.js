446c098a06a5cd42c9716408f0d88be8bcf06d51
nkunic <natasa.kunic@gmail.com>
2022-04-14 15:44:56 +0200

    2 files modified

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
 import './style.css';
 
 // Write Javascript code!
+
+var car = {
+  name: 'Volvo',
+  speed: 260,
+  info: function () {
+    console.log(car.name + ' ' + car.speed);
+  },
+};
+
+car.info();
--- js/07-functions.js
+++ js/07-functions.js
@@ -1,61 +1,85 @@
 /***** FUNCTION *****/
 function display() {
- var name = 'Js';
- console.log(name);
+  var name = 'Js';
+  console.log(name);
 }
 display();
 
 function display() {
-console.log(name2);
+  console.log(name2);
 }
 display(); // name2 is not defined
 
-function display(a){
- console.log(a);
+function display(a) {
+  console.log(a);
 }
 display(); // undefined
 
 function display(a) {
- console.log(a);
+  console.log(a);
 }
 display('Javascript');
 display('Php');
 
-function info(){
-	console.log('Zdravo, Svete');
+function info() {
+  console.log('Zdravo, Svete');
 }
 info();
 
-function zbir (broj1,broj2){
-	console.log(broj1+broj2);
+function zbir(broj1, broj2) {
+  console.log(broj1 + broj2);
 }
-zbir(31,90)
+zbir(31, 90);
 
 function naKvadrat(broj1) {
- var rezultat = broj1 * broj1;
- return rezultat;
+  var rezultat = broj1 * broj1;
+  return rezultat;
 }
 console.log(naKvadrat(25)); // 625
 var brojNaKvadrat = naKvadrat(25);
 console.log(brojNaKvadrat); // 625
 
 function pozdrav(ime) {
- var ceoPozdrav = 'Zdravo' + ' ' + ime + '!';
- return ceoPozdrav;
+  var ceoPozdrav = 'Zdravo' + ' ' + ime + '!';
+  return ceoPozdrav;
 }
 console.log(pozdrav('Juliska'));
 var pozdravImenom = pozdrav('Juliska');
 console.log(pozdravImenom);
 
-function x(a,b){
-  console.log(a+b)
+function x(a, b) {
+  console.log(a + b);
 }
-x("3",1212);
-
+x('3', 1212);
 
 var x = 10;
-function pokusaj(){
-	x = 20;
+function pokusaj() {
+  x = 20;
 }
 pokusaj();
 console.log(x); // 20
+
+/* Function statement */
+info1();
+function info1() {
+  console.log('Zdravo');
+}
+info1();
+
+/* Function expression (nema ime, anonimna funkcija, njeno ime je ime varijable) */
+info2(); // info2 is not a function
+var info2 = function () {
+  console.log('Zdravo opet');
+};
+info2();
+
+
+/* Metode */
+var car = {
+  name: "Volvo",
+  speed: 260,
+  info: function(){
+    console.log(car.name + " " + car.speed);
+  }
+}
+car.info(); // Volvo 260
