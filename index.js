ab4464476fcee740e5e8e5f7b9c0c525bbe0f9ce
nkunic <natasa.kunic@gmail.com>
2022-04-19 11:56:39 +0200

    3 files modified

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
 import './style.css';
 
 // Write Javascript code! //
+
+/* Tag list item */
+var allLi = document.querySelectorAll('li'); // array
+console.log(allLi);
+
+var oneLi = document.querySelectorAll('li'); // Ist element
+console.log(oneLi);
+
+var li = document.querySelectorAll('ul > li');
+console.log(li);
--- js/11-dom.js
+++ js/11-dom.js
@@ -1,3 +1,4 @@
+/* INTRODUCTION */
 console.log(window);
 console.log(window.document);
 console.log(window.document.children);
@@ -13,3 +14,33 @@ var title = window.document.children[0].children[1].children[0].children[0];
 console.log(title);
 title.style.color = 'red';
 title.style.fontSize = '100px';
+
+/* SELECTORS */
+
+/* Id */
+var app = document.getElementById('app'); // element
+console.log(app);
+
+/* Tag */
+var title = document.getElementsByTagName('h1'); // array
+console.log(title);
+
+/* Class */
+var button = document.getElementsByClassName('button'); // array
+console.log(button);
+
+/* Input name */
+var car = document.getElementsByName('car'); // array
+console.log(car);
+
+/* Tag list item */
+var allLi = document.querySelectorAll('li'); // array
+console.log(allLi);
+
+var oneLi = document.querySelectorAll('li'); // Ist element
+console.log(oneLi);
+
+var li = document.querySelectorAll('ul > li'); 
+console.log(li);
+
+/* ATRIBUTES */
