331bd95a3049d3ccc5d9f1d3a178a99499648b39
nkunic <natasa.kunic@gmail.com>
2022-04-19 13:47:22 +0200

    1 file modified

--- index.js
+++ index.js
@@ -3,9 +3,3 @@ import './style.css';
 
 // Write Javascript code! //
 
-var allList = document.getElementsByTagName('li');
-
-var ul = document.getElementsByTagName('ul')[0];
-ul.setAttribute('title', 'Ovo je naša lista');
-
-console.log(ul.getAttribute('title'));
