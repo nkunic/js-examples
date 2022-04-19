2dc971aaf092c9e09ec92087707e58be56a16176
nkunic <natasa.kunic@gmail.com>
2022-04-19 14:07:41 +0200

    1 file modified

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 import './style.css';
 
 // Write Javascript code! //
-var element = document.getElementsByTagName('h2');
-console.log(element);
+//var element = document.getElementsByTagName('h2')[0];
+//console.log(element);
 //element.id.subtitle;
 //element.id("subtitle");
-element.id = "subtitle";
+//element.id = "subtitle";
 //element.setId("naslov");
+
+let element = document.getElementsByTagName("h2")[0];
+console.log(element);
+element.id = "newid";
