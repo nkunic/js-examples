1f970b38e6c84e5f99e82d61b6f2280aa4f3a4a3
nkunic <natasa.kunic@gmail.com>
2022-04-20 17:47:53 +0200

    4 files modified

--- index.js
+++ index.js
@@ -3,13 +3,3 @@ import './style.css';
 
 // Write Javascript code! //
 
-var btn = document.getElementsByTagName('button')[0];
-
-btn.addEventListener('click', function(e) {
-  console.log(e); // Objekat: Mouse PointerEvent
-});
-
-window.addEventListener('mousemove', function(e) {
-  console.clear();
-  console.log("x:" + e.x + "   y: " + e.y);
-});
--- js/12-events.js
+++ js/12-events.js
@@ -135,6 +135,26 @@ obj.info();
 
 /*------------- EVENT OBJECT ---------------*/
 
+<button>Start</button>
+var btn = document.getElementsByTagName('button')[0];
+btn.addEventListener('click', function (e) {
+  console.log(e); // Objekat: Mouse PointerEvent
+});
+window.addEventListener('mousemove', function (e) {
+  console.clear();
+  console.log('x:' + e.x + '   y:' + e.y); // x:108 y:183
+});
+
+<div class="box"></div>
+var box = document.querySelector('.box');
+box.addEventListener('click', function (e) {
+  if (e.altKey === true) {
+    this.style.background = 'tomato';
+  } else {
+    this.style.background = 'cadetblue';
+  }
+});
+
 /*------------- PREVENT DEFAULT ---------------*/
 
 <a id="a1" href="https://www.krojacevaskola.com/">Link</a>
