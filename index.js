53e50f8f2b9119ef25a03c491982ef2ed5b45c27
nkunic <natasa.kunic@gmail.com>
2022-04-20 22:17:35 +0200

    3 files modified

--- index.js
+++ index.js
@@ -3,14 +3,4 @@ import './style.css';
 
 // Write Javascript code! //
 
-var xo = document.querySelector('.xo');
 
-function createTable() {
-  var text = '';
-  for (var i = 0; i < 9; i++) {
-    text += '<div class="xo__item"></div>';
-  }
-  xo.innerHTML = text;
-}
-
-createTable();
