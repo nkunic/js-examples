4691f394e944d5508e73689980d3bc20dae3da92
nkunic <natasa.kunic@gmail.com>
2022-04-20 12:12:47 +0200

    1 file modified

--- index.js
+++ index.js
@@ -3,25 +3,4 @@ import './style.css';
 
 // Write Javascript code! //
 
-var wrapp = document.querySelector('.wrapp');
 
-var text = 'Zdravo iz Javascripta';
-function typing() {
-
-  var textArrayTogether = text.split(); /// All together in array
-  console.log(textArrayTogether);
-
-  var textArrayAlone = text.split(""); // Split alone in array
-  console.log(textArrayAlone);
-
-  var loop = setInterval(function() {
-    if(textArrayAlone.length !== 0) {
-      //wrapp.innerHTML = textArrayAlone.shift(); // One by one letter
-      wrapp.innerHTML += textArrayAlone.shift(); // All word
-    } else {
-      clearInterval(loop);
-    }
-  },250)
-
-}
-typing();
