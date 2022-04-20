943827a07f29e6cef55c8b3a4e3094a380e24845
nkunic <natasa.kunic@gmail.com>
2022-04-20 15:58:56 +0200

    1 file modified

--- index.js
+++ index.js
@@ -6,12 +6,9 @@ import './style.css';
 var btn = document.getElementsByTagName('button')[0];
 
 
-
-
-// var a1 = document.getElementById(a1);
-// document.a1.addEventListener("click", function(event){
-//     	alert('Kliknuli ste na naslov');
-// });
+document.btn.addEventListener("click", function(event){
+    	alert('Kliknuli ste na naslov');
+});
 
 // document.getElementById("a1").addEventListener("click", function(event){
 //   event.preventDefault()
